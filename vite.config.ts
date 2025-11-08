88a3c9b769ffc71911bc47401e5c45955ded93bf
Motlagh <moti.parsa.141@gmail.com>
2025-11-08 09:21:27 +0330

    fix(vite.config.ts):
    fix for vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
@@ -6,41 +6,35 @@ export default defineConfig({
 
     build: {
         chunkSizeWarningLimit: 600,
+        cssCodeSplit: true,
 
         rollupOptions: {
             output: {
+                chunkFileNames: 'assets/[name]-[hash].js',
+
                 manualChunks(id) {
                     if (id.includes('node_modules')) {
                         if (id.includes('react') || id.includes('react-dom')) {
                             return 'react-vendor';
                         }
-
                         if (id.includes('i18next') || id.includes('react-i18next')) {
                             return 'i18n-vendor';
                         }
-
                         if (id.includes('chart.js')) {
                             return 'chart-vendor';
                         }
-
                         if (id.includes('lodash')) {
                             return 'lodash-vendor';
                         }
-
                         if (id.includes('dayjs')) {
                             return 'dayjs-vendor';
                         }
-
                         if (id.includes('@mui') || id.includes('antd')) {
                             return 'ui-vendor';
                         }
-
                         return 'misc-vendor';
                     }
                 },
-
-                chunkFileNames: 'assets/[name]-[hash].js',
-                entryFileNames: 'assets/[name]-[hash].js',
             },
         },
     },
